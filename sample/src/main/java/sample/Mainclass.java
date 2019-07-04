51ac75bc47668261a0381f954192dfeb03b2a010
pputhala <pputhala@DIN78000421.corp.capgemini.com>
2019-07-04 12:59:15 +0530

    This is my first commit

---
+++ sample/src/main/java/sample/Mainclass.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Mainclass {
+	public static void main(String args[])
+	{
+		System.out.println("first element in an array");
+
+           System.out.println("first element in an array");
+}
+}
